bfca03058561a90df604afabe58f1b2c468a5583
darcrand <darcrandex@gmail.com>
2022-02-21 10:31:14 +0800

    ✨ feat: 更新用户模块

--- server/libs/db/src/models/user.model.ts
+++ server/libs/db/src/models/user.model.ts
@@ -1,6 +1,14 @@
 import { Prop } from '@typegoose/typegoose'
 
+export enum Roles {
+  Admin = 'admin',
+  Account = 'account',
+}
+
 export class User {
+  @Prop({ required: true })
+  role!: Roles
+
   @Prop({ required: true })
   openid!: string
 
--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { User } from '@app/db/models/user.model'
+import { Roles, User } from '@app/db/models/user.model'
 import { Body, Controller, Post } from '@nestjs/common'
 import { ReturnModelType } from '@typegoose/typegoose'
 import { InjectModel } from 'nestjs-typegoose'
@@ -10,9 +10,18 @@ export class UserController {
     @InjectModel(User) readonly userModel: ReturnModelType<typeof User>,
   ) {}
 
+  @Post('/sign-up')
+  async create(@Body() createUserDto: User) {
+    const user = await this.userModel.create({
+      ...createUserDto,
+      role: Roles.Account,
+    })
+    return { user }
+  }
+
   @Post('/login')
   async login(@Body() body: { code: string }) {
-    const res = await axios.get<{ openid: string; session_key: string }>(
+    const res = await axios.get<{ openid?: string; session_key: string }>(
       'https://api.weixin.qq.com/sns/jscode2session',
       {
         params: {
@@ -26,11 +35,5 @@ export class UserController {
 
     const user = await this.userModel.findOne({ openid: res.data.openid })
     return { user: user || null, openid: res.data.openid }
-  }
-
-  @Post('/sign-up')
-  async create(@Body() createUserDto: User) {
-    const user = await this.userModel.create(createUserDto)
-    return { user }
   }
 }
