143acf7ea6ad432a3842b77c67a16f6327136e0b
darcrand <darcrandex@gmail.com>
2022-05-10 10:50:50 +0800

    🎈 perf: 样式优化

--- mp-react/src/components/TopHeader/index.tsx
+++ mp-react/src/components/TopHeader/index.tsx
@@ -9,21 +9,23 @@ import { useSafeArea } from '@/stores/use-safe-area'
 import Icon from '@/components/Icon'
 
 import iconBack from '@/assets/icons/icon-back.svg'
+import { mergeClassNames } from '@/utils'
 
 export const HEADER_BOTTOM = 5
-const iconSize = 20
+const iconSize = 18
 
 export interface TopHeaderProps {
   showLeft?: boolean
+  className?: string
 }
 
-const TopHeader: React.FC<TopHeaderProps> = ({ showLeft = true, children }) => {
+const TopHeader: React.FC<TopHeaderProps> = ({ showLeft = true, className, children }) => {
   const { safeArea } = useSafeArea()
 
   return (
     <>
       <header
-        className='px-4 text-gray-800 shadow-s'
+        className={mergeClassNames('px-4 text-gray-800', className)}
         style={{
           paddingTop: safeArea.menuBtnRect.top,
           paddingBottom: HEADER_BOTTOM
@@ -37,7 +39,7 @@ const TopHeader: React.FC<TopHeaderProps> = ({ showLeft = true, children }) => {
           )}
 
           {/* children 作为标题文本 */}
-          <span className='text-sm text-gray-800'>{children}</span>
+          <span className='text-sm text-gray-800 font-bold'>{children}</span>
 
           <i style={{ width: iconSize }}></i>
         </div>
--- mp-react/src/components/TopSearch/index.tsx
+++ mp-react/src/components/TopSearch/index.tsx
@@ -4,9 +4,12 @@
  * @author darcrand
  */
 
+import React from 'react'
 import { useSafeArea } from '@/stores/use-safe-area'
 import { mergeClassNames } from '@/utils'
-import React from 'react'
+import Icon from '@/components/Icon'
+
+import iconSearch from '@/assets/icons/icon-search-gray-400.svg'
 
 export const HEADER_BOTTOM = 8
 
@@ -32,8 +35,14 @@ const TopSearch: React.FC<TopSearchProps> = ({ children = '落叶，诉说着故
         style={{ paddingTop: safeArea.menuBtnRect.top, paddingBottom: HEADER_BOTTOM }}
         onClick={onClick}
       >
-        <div style={{ width: safeArea.menuBtnRect.left, height: safeArea.menuBtnRect.height }}>
-          <p className='bg-gray-100 mx-4 px-4 py-2 rounded-full text-xs text-gray-400 box-border'>{children}</p>
+        <div style={{ width: safeArea.menuBtnRect.left }}>
+          <p
+            className='flex items-center bg-gray-100 mx-4 px-2 rounded-full text-xs text-gray-400 box-border'
+            style={{ height: safeArea.menuBtnRect.height, lineHeight: `${safeArea.menuBtnRect.height}px` }}
+          >
+            <Icon url={iconSearch} size={20} />
+            <span className='ml-1'>{children}</span>
+          </p>
         </div>
       </header>
     </>
