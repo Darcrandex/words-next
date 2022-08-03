fdcad3ec530e3a52a77f8efb1750502a9d95dfd6
darcrand <darcrandex@gmail.com>
2022-08-03 14:34:21 +0800

    🌈 style: 改改样式

--- mp-react/src/pages/about/index.tsx
+++ mp-react/src/pages/about/index.tsx
@@ -4,13 +4,20 @@
  * @author darcrand
  */
 
+import { Image } from '@tarojs/components'
 import TopHeader from '@/components/TopHeader'
 
 const About: React.FC = () => {
   return (
     <>
       <TopHeader>关于</TopHeader>
-      <h1>About</h1>
+
+      <Image src='' className='block mx-auto my-4 w-32 h-32 rounded-full bg-gray-100' />
+
+      <p className='m-4 text-gray-600 indent-1'>
+        <code className='inline-block font-bold'>Just Words（片语）</code>
+        是一款个人开发的小程序。在这里可以每天遇到一句让你耳目一新的句子。希望你喜欢。
+      </p>
     </>
   )
 }
--- mp-react/src/pages/mine/index.tsx
+++ mp-react/src/pages/mine/index.tsx
@@ -8,9 +8,8 @@ import { Image } from '@tarojs/components'
 import BottomTabNavs from '@/containers/BottomTabNavs'
 import Icon from '@/components/Icon'
 import { useUser } from '@/stores/use-user'
-import { navigateToPage } from '@/utils'
+import { mergeClassNames, navigateToPage } from '@/utils'
 
-import AuthWrapper from '@/containers/AuthWrapper'
 import iconMedal from '@/assets/icons/icon-medal.svg'
 import iconIdea from '@/assets/icons/icon-idea.svg'
 import iconArrowRight from '@/assets/icons/icon-arrow-right.svg'
@@ -19,6 +18,12 @@ import iconAbout from '@/assets/icons/icon-about-theme.svg'
 import iconSetting from '@/assets/icons/icon-setting-theme.svg'
 
 import './style.less'
+
+const menus = [
+  { icon: iconCollection, title: '我的收藏', to: 'collection' },
+  { icon: iconAbout, title: '关于', to: 'about' },
+  { icon: iconSetting, title: '设置', to: 'settings' }
+]
 
 const Mine: React.FC = () => {
   const { info, hadLogined, signUp } = useUser()
@@ -55,30 +60,18 @@ const Mine: React.FC = () => {
         </div>
       </section>
 
-      <section className='m-4 p-2 rounded shadow-m bg-white'>
-        <AuthWrapper onClick={() => navigateToPage('collection')}>
-          <div className='flex items-center mx-2 py-2 link-item'>
-            <Icon url={iconCollection} size={24} />
-            <span className='ml-2 mr-auto text-gray-800 text-sm'>我的收藏</span>
+      <section className='m-4 py-2 rounded shadow-m bg-white'>
+        {menus.map((v, i) => (
+          <div
+            key={v.to}
+            className={mergeClassNames('flex items-center mx-4 py-2', i !== menus.length - 1 && 'border-b')}
+            onClick={() => navigateToPage(v.to)}
+          >
+            <Icon url={v.icon} size={24} />
+            <span className='ml-2 mr-auto text-gray-800 text-sm'>{v.title}</span>
             <Icon url={iconArrowRight} size={16} />
           </div>
-        </AuthWrapper>
-
-        <AuthWrapper onClick={() => navigateToPage('about')}>
-          <div className='flex items-center mx-2 py-2 link-item'>
-            <Icon url={iconAbout} size={24} />
-            <span className='ml-2 mr-auto text-gray-800 text-sm'>关于</span>
-            <Icon url={iconArrowRight} size={16} />
-          </div>
-        </AuthWrapper>
-
-        <AuthWrapper onClick={() => navigateToPage('settings')}>
-          <div className='flex items-center mx-2 py-2'>
-            <Icon url={iconSetting} size={24} />
-            <span className='ml-2 mr-auto text-gray-800 text-sm'>设置</span>
-            <Icon url={iconArrowRight} size={16} />
-          </div>
-        </AuthWrapper>
+        ))}
       </section>
 
       <BottomTabNavs />
